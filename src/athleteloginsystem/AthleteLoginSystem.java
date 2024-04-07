212b28abb45b33cfe18f3066e9c3503c9cbf469e
Martin Serdenia <martinserdenia@gmail.com>
2024-04-07 13:24:38 +0800

    init

---
+++ src/athleteloginsystem/AthleteLoginSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package athleteloginsystem;
+
+
+/**
+ *
+ * @author Administrator
+ */
+public class AthleteLoginSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
